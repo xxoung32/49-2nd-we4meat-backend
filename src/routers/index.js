4b8fff4d28e36e134c2aa03fd7a0e724e0aa5e77
Rooney Lee <kroonstazy@gmail.com>
2023-10-02 20:26:09 +0900

    chore: index 수정

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const userRouter = require('./userRouter');
-const productRouter = require('./productRouter')
+const productRouter = require('./productRouter');
+// const reviewRouter = require('./reviewRouter');
 
 router.use('/users', userRouter);
 router.use('/list', productRouter);
+// router.use('/review', reviewRouter);
 
 module.exports = router;
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { productController } = require('../controllers');
-const { listProductController } = productController;
+const { listProductController, productDetailController } = productController;
 const router = express.Router();
 
 router.get('/', listProductController);
+router.get('/detail', productDetailController);
 
 module.exports = router;
