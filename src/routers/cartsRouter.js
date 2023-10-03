3bf8c9a7c6f3a4ca71a35d513bbd28c4c9e3a377
Rooney Lee <kroonstazy@gmail.com>
2023-10-03 23:25:42 +0900

    fix: 라우터에 verifyToken 적용

--- src/routers/cartsRouter.js
+++ src/routers/cartsRouter.js
@@ -2,22 +2,20 @@ const express = require('express');
 const { verifyToken } = require('../../middleware');
 const { cartsController } = require('../controllers');
 const {
-insertCartsController,
-getCartsByCustomerIdController,
-deleteCartByIdController
+  insertCartsController,
+  getCartsByCustomerIdController,
+  deleteCartByIdController,
 } = cartsController;
 const router = express.Router();
 
-// ====> 토큰 postman에 적용하는 법 몰라서 통신을 위해 verrfyToken 함수 뺐음
-router.post('/create', insertCartsController);  
-router.get('/:id', getCartsByCustomerIdController);
-router.patch('/delete', deleteCartByIdController);
+// // ====> 토큰 postman에 적용하는 법 몰라서 통신을 위해 verrfyToken 함수 뺐음
+// router.post('/create', insertCartsController);
+// router.get('/:id', getCartsByCustomerIdController);
+// router.patch('/delete', deleteCartByIdController);
 
 // ====> 토큰 발행 후 사용할 routers
-// router.post('/create', verifyToken, insertCartsController);
-// router.get('/:id', verifyToken, getCartsByCustomerIdController);
-// router.patch('/delete',verifyToken, deleteCartByIdController);
-
-
+router.post('/create', verifyToken, insertCartsController);
+router.get('/:id', verifyToken, getCartsByCustomerIdController);
+router.patch('/delete',verifyToken, deleteCartByIdController);
 
 module.exports = router;
