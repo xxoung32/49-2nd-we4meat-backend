b8227be0ab4af731df5951aff8d530538a82340a
Rooney Lee <kroonstazy@gmail.com>
2023-10-05 07:59:36 +0900

    feat: 상품 개별 추가시 장바구니 확인 로직 추가 by 아영

--- src/models/cartDao.js
+++ src/models/cartDao.js
@@ -14,10 +14,10 @@ const updateItemDao = async (customerId, productId, quantity) => {
 
 // 장바구니 조회 - 완
 const getCartDao = async (customerId) => {
-  const status = 1; 
-      // 처음에 작성한 코드는 status를 query문에 where절을 사용하여 데이터 필터링을 했는데,
-      // 수정 된 코드는 status를 상수로 선언하고 값을 1로 주는 방법으로 수정
-      // status를 상수로 선언을 하게 되면 where절을 사용해서 필터링 하는 것보다 가독성과 유지보수성이 더 좋아진다.
+  const status = 1;
+  // 처음에 작성한 코드는 status를 query문에 where절을 사용하여 데이터 필터링을 했는데,
+  // 수정 된 코드는 status를 상수로 선언하고 값을 1로 주는 방법으로 수정
+  // status를 상수로 선언을 하게 되면 where절을 사용해서 필터링 하는 것보다 가독성과 유지보수성이 더 좋아진다.
   return await dataSource.query(
     `
     SELECT p.id AS productId,
@@ -31,7 +31,7 @@ const getCartDao = async (customerId) => {
     JOIN carts AS cart ON c.id = cart.customer_id
     JOIN products AS p ON cart.product_id = p.id
     WHERE cart.status = ? AND c.id = ? `,
-    [status, customerId],    // ===> 장바구니에는 수량*가격만 필요한데 개별가격이 필요한 이유를 모르겠는데 뭔가요....?
+    [status, customerId],
   );
 };
 
@@ -48,8 +48,8 @@ const purgeCartDao = async (customerId) => {
 };
 
 //개별 아이템 추가할 때 기존 아이템 있는 지 확인 있으면 수량 추가
-const existingItemCheckDao = async(customerId, productId) => {
- // 기존 장바구니 있는 지 확인
+const existingItemCheckDao = async (customerId, productId) => {
+  // 기존 장바구니 있는 지 확인
   const status = 1;
   return await dataSource.query(
     `
@@ -57,26 +57,26 @@ const existingItemCheckDao = async(customerId, productId) => {
     FROM carts
     WHERE customer_id = ? AND product_id = ? AND status = ?
     `,
-    [customerId, productId, status]
-    );
-  };
- 
-  // 같은 상품이 있는 경우 수량 추가
-  const updateQuantityDao = async (cartId, quantity) => {
-    return await dataSource.query(`
+    [customerId, productId, status],
+  );
+};
+
+// 같은 상품이 있는 경우 수량 추가
+const updateQuantityDao = async (cartId, quantity) => {
+  return await dataSource.query(
+    `
       UPDATE carts
       SET quantity = quantity + ?
       WHERE id = ?;
-    `, [quantity, cartId]);
-  };
-  
-    
+    `,
+    [quantity, cartId],
+  );
+};
 
 module.exports = {
   updateItemDao,
   getCartDao,
   purgeCartDao,
   existingItemCheckDao,
-  updateQuantityDao
+  updateQuantityDao,
 };
-
--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,12 +1,17 @@
 const { cartDao } = require('../models');
-const { updateItemDao, getCartDao, purgeCartDao, existingItemCheckDao,updateQuantityDao } = cartDao;
-
+const {
+  updateItemDao,
+  getCartDao,
+  purgeCartDao,
+  existingItemCheckDao,
+  updateQuantityDao,
+} = cartDao;
 
 // 장바구니 개별 아이템 (제품) 추가 - 완
 // 상품리스트 페이지에서 개별 아이템을 추가할 수 있어서 addItemService 함수를 추가
 const addItemService = async (customerId, productId, quantity) => {
   const existingCartItem = await existingItemCheckDao(customerId, productId);
-  console.log("existingCartItem: ", existingCartItem);
+  console.log('existingCartItem: ', existingCartItem);
   if (existingCartItem.length > 0) {
     // 이미 장바구니에 해당 제품이 있으면 수량 업데이트
     const cartId = existingCartItem[0].id;
